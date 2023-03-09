8813912f0b742882f8c237a71899f9961c3c05ab
Claude Agnes <rlawnstjddl12@gmail.com>
2023-03-09 13:30:01 +0900

    Upload FIles
    Upload FIles

---
+++ Bat_to_exe/Bat_to_exe/Form1.Designer.cs
@@ -0,0 +1,119 @@
+﻿namespace Bat_to_exe
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.metroButton1 = new MetroFramework.Controls.MetroButton();
+            this.metroTextBox1 = new MetroFramework.Controls.MetroTextBox();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.metroButton2 = new MetroFramework.Controls.MetroButton();
+            this.SuspendLayout();
+            // 
+            // metroButton1
+            // 
+            this.metroButton1.Location = new System.Drawing.Point(398, 63);
+            this.metroButton1.Name = "metroButton1";
+            this.metroButton1.Size = new System.Drawing.Size(68, 26);
+            this.metroButton1.TabIndex = 0;
+            this.metroButton1.Text = ". . .";
+            this.metroButton1.UseSelectable = true;
+            this.metroButton1.Click += new System.EventHandler(this.metroButton1_Click);
+            // 
+            // metroTextBox1
+            // 
+            // 
+            // 
+            // 
+            this.metroTextBox1.CustomButton.Image = null;
+            this.metroTextBox1.CustomButton.Location = new System.Drawing.Point(53, 1);
+            this.metroTextBox1.CustomButton.Name = "";
+            this.metroTextBox1.CustomButton.Size = new System.Drawing.Size(21, 21);
+            this.metroTextBox1.CustomButton.Style = MetroFramework.MetroColorStyle.Blue;
+            this.metroTextBox1.CustomButton.TabIndex = 1;
+            this.metroTextBox1.CustomButton.Theme = MetroFramework.MetroThemeStyle.Light;
+            this.metroTextBox1.CustomButton.UseSelectable = true;
+            this.metroTextBox1.CustomButton.Visible = false;
+            this.metroTextBox1.Enabled = false;
+            this.metroTextBox1.Lines = new string[0];
+            this.metroTextBox1.Location = new System.Drawing.Point(23, 63);
+            this.metroTextBox1.MaxLength = 32767;
+            this.metroTextBox1.Name = "metroTextBox1";
+            this.metroTextBox1.PasswordChar = '\0';
+            this.metroTextBox1.ScrollBars = System.Windows.Forms.ScrollBars.None;
+            this.metroTextBox1.SelectedText = "";
+            this.metroTextBox1.SelectionLength = 0;
+            this.metroTextBox1.SelectionStart = 0;
+            this.metroTextBox1.ShortcutsEnabled = true;
+            this.metroTextBox1.Size = new System.Drawing.Size(369, 23);
+            this.metroTextBox1.TabIndex = 1;
+            this.metroTextBox1.UseSelectable = true;
+            this.metroTextBox1.WaterMarkColor = System.Drawing.Color.FromArgb(((int)(((byte)(109)))), ((int)(((byte)(109)))), ((int)(((byte)(109)))));
+            this.metroTextBox1.WaterMarkFont = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Pixel);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(23, 92);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(443, 299);
+            this.richTextBox1.TabIndex = 2;
+            this.richTextBox1.Text = "";
+            // 
+            // metroButton2
+            // 
+            this.metroButton2.Location = new System.Drawing.Point(337, 397);
+            this.metroButton2.Name = "metroButton2";
+            this.metroButton2.Size = new System.Drawing.Size(129, 30);
+            this.metroButton2.TabIndex = 3;
+            this.metroButton2.Text = "Convert to exe";
+            this.metroButton2.UseSelectable = true;
+            this.metroButton2.Click += new System.EventHandler(this.metroButton2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(489, 450);
+            this.Controls.Add(this.metroButton2);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.metroTextBox1);
+            this.Controls.Add(this.metroButton1);
+            this.Name = "Form1";
+            this.Text = "Bat_to_exe";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private MetroFramework.Controls.MetroButton metroButton1;
+        private MetroFramework.Controls.MetroTextBox metroTextBox1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private MetroFramework.Controls.MetroButton metroButton2;
+    }
+}
+
---
+++ Bat_to_exe/Bat_to_exe/Form1.cs
@@ -0,0 +1,89 @@
+﻿using Microsoft.CSharp;
+using System;
+using System.CodeDom.Compiler;
+using System.IO;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Bat_to_exe
+{
+    public partial class Form1 : MetroFramework.Forms.MetroForm
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            metroButton2.Enabled = false;
+        }
+
+        private void metroButton1_Click(object sender, EventArgs e)
+        {
+            
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Title = "Batch File Select";
+            ofd.Filter = "Batch File (*.bat) | *.bat| All Files (*.*) | *.*";
+            if(ofd.ShowDialog() == DialogResult.OK)
+            {
+                metroTextBox1.Text = ofd.FileName;
+                richTextBox1.Text = File.ReadAllText(ofd.FileName);
+                metroButton2.Enabled = true;
+            }
+        }
+
+        private void metroButton2_Click(object sender, EventArgs e)
+        {
+            var replacement = richTextBox1.Text.Replace("\"", "\"\"");
+            replacement = Convert.ToBase64String(Encoding.UTF8.GetBytes(replacement));
+            string code = @"
+using System;
+using System.Diagnostics;
+using System.IO;
+namespace Program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var batch_code = @" + "\"" + Encoding.UTF8.GetString(Convert.FromBase64String(replacement)) + "\";" + @"
+            var temp_file = Path.GetTempFileName() + "".bat"";
+            File.WriteAllText(temp_file, batch_code);
+            var process = new Process();
+            process.StartInfo.FileName = temp_file;
+            process.StartInfo.UseShellExecute = false;
+            process.StartInfo.RedirectStandardError = true;
+            process.Start();
+            process.WaitForExit();
+            File.Delete(temp_file);
+        }
+    }
+}
+";
+            MessageBox.Show(code);
+
+            string savePath = string.Empty;
+            SaveFileDialog saveFile = new SaveFileDialog();
+            saveFile.Title = "Save Location";
+            saveFile.Filter = "exe File(*.exe) |*.exe";
+            if(saveFile.ShowDialog() == DialogResult.OK)
+            {
+                savePath = saveFile.FileName.ToString();
+                CodeDomProvider codeDomProvider = new CSharpCodeProvider();
+                CompilerParameters compilerParameters = new CompilerParameters();
+                compilerParameters.ReferencedAssemblies.Add("System.dll");
+
+                compilerParameters.GenerateExecutable = true;
+                compilerParameters.OutputAssembly = savePath;
+                CompilerResults compilerResults = codeDomProvider.CompileAssemblyFromSource(compilerParameters, code);
+                if (compilerResults.Errors.Count > 0)
+                {
+                    richTextBox1.Text = compilerResults.Errors[0].ToString();
+                }
+                else
+                {
+                    MessageBox.Show("Success");
+                    richTextBox1.Text = string.Empty;
+                }
+            }
+            
+        }
+    }
+}
